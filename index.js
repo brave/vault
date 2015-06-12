f6459b25b3912d4332e7ea150a56a4d4d2273594
Kevin Grandon <kevingrandon@yahoo.com>
2015-06-12 15:20:14 -0700

    Use port from environment define.

--- index.js
+++ index.js
@@ -7,18 +7,20 @@ var intents = require('./controllers/intents');
 var adManifest = require('./controllers/ad-manifest');
 var sync = require('./controllers/sync');
 
+var PORT = process.env.PORT || 3000;
+
 app.use(logger());
 
 app.use(route.get('/', function * () {
-	this.body = 'Welcome to the Vault.';
+  this.body = 'Welcome to the Vault.';
 }));
 app.use(route.get('/ad-manifest', adManifest.get));
 app.use(route.post('/intents', intents.push));
 app.use(route.get('/sync/:userId', sync.get));
 app.use(route.post('/sync', sync.push));
 
-app.listen(3000, function() {
-  debug('webserver started at http://localhost:3000');
+app.listen(PORT, function() {
+  debug('webserver started on port 3000');
 });
 
 app.on('error', function(err){
