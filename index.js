66ac97a01852aaaf50b97b142c435a4b5e198440
Kevin Grandon <kevingrandon@yahoo.com>
2015-06-12 11:39:44 -0700

    Add a simple sync controller.

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ var logger = require('koa-logger');
 var app = koa();
 var intents = require('./controllers/intents');
 var adManifest = require('./controllers/ad-manifest');
+var sync = require('./controllers/sync');
 
 app.use(logger());
 
 app.use(route.get('/ad-manifest', adManifest.get));
 app.use(route.post('/intents', intents.push));
+app.use(route.get('/sync/:userId', sync.get));
+app.use(route.post('/sync', sync.push));
 
 app.listen(3000, function() {
     console.log('Koa is listening to http://localhost:3000');
