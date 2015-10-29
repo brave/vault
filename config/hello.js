cc3e8bf0c23a29a34bb8561ac5e1bd5ea4ebf1a9
Marshall Rose <mrose17@gmail.com>
2015-10-29 15:28:16 -0700

    typo... grrrr!

--- config/hello.js
+++ config/hello.js
@@ -3,6 +3,6 @@ module.exports =
   world: '/v1/oip/ads/statistics',
   bye: 'https://brave.com',
   clientId: process.env.GITHUB_CLIENT_ID || '5df62e56d1075b7a6e05',
-  clientSecret: process.env.GITHUB_CLIENT_SECRET || 'a79bdf99daee2e383ac7d39648842a234a0d9f63'
+  clientSecret: process.env.GITHUB_CLIENT_SECRET || 'a79bdf99daee2e383ac7d39648842a234a0d9f63',
   isSecure: process.env.GITHUB_FORCE_HTTPS || false
 }
