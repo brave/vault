c3b3cf209dcd1ef17e7e9cdb4ab61aceab75d65a
Marshall Rose <mrose17@gmail.com>
2015-11-02 09:53:39 -0800

    [github] remove unused configuration file

--- config/hello.js
+++
@@ -1,8 +0,0 @@
-module.exports =
-{ organization: 'brave',
-  world: '/v1/oip/ads/statistics?format=on',
-  bye: 'https://brave.com',
-  clientId: process.env.GITHUB_CLIENT_ID || '5df62e56d1075b7a6e05',
-  clientSecret: process.env.GITHUB_CLIENT_SECRET || 'a79bdf99daee2e383ac7d39648842a234a0d9f63',
-  isSecure: process.env.GITHUB_FORCE_HTTPS || false
-}
