e9c436458049698a7000d7c02f35ad3300ecd74c
Marshall Rose <mrose17@gmail.com>
2016-04-18 14:25:47 -0700

    config update

--- config/config.development.js.tpl
+++ config/config.development.js.tpl
@@ -1,13 +1,14 @@
 module.exports =
-{ port: process.env.PORT || 3000,
-  database: process.env.MONGO_URI || 'localhost/test',
-  login:
-  { organization: 'brave',
-    world: '/documentation',
-    bye: 'https://brave.com',
-    clientId: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    ironKey: process.env.IRON_KEYPASS,
-    isSecure: process.env.GITHUB_FORCE_HTTPS || false
+{ port: process.env.PORT || 3000
+, database       :
+  { mongo        : process.env.MONGO_URI || 'localhost/test' }
+,  login:
+  { organization : 'brave'
+  , world        : '/documentation'
+  , bye          : 'https://brave.com'
+  , clientId     : process.env.GITHUB_CLIENT_ID
+  , clientSecret : process.env.GITHUB_CLIENT_SECRET
+  , ironKey      : process.env.IRON_KEYPASS
+  , isSecure     : process.env.GITHUB_FORCE_HTTPS || false
   }
-};
+}
--- config/config.test.js
+++ config/config.test.js
@@ -1,13 +1,14 @@
 module.exports =
-{ port: process.env.PORT || 3000,
-  database: process.env.MONGO_URI || 'localhost/test',
-  login:
-  { organization: 'brave',
-    world: '/documentation',
-    bye: 'https://brave.com',
-    clientId: process.env.GITHUB_CLIENT_ID || '00000000000000000000',
-    clientSecret: process.env.GITHUB_CLIENT_SECRET || '0000000000000000000000000000000000000000',
-    ironKey: process.env.IRON_KEYPASS,
-    isSecure: process.env.GITHUB_FORCE_HTTPS || false
+{ port           : process.env.PORT || 3000
+, database       :
+  { mongo        : process.env.MONGO_URI || 'localhost/test' }
+, login:
+  { organization : 'brave'
+  , world        : '/documentation'
+  , bye          : 'https://brave.com'
+  , clientId     : process.env.GITHUB_CLIENT_ID || '00000000000000000000'
+  , clientSecret : process.env.GITHUB_CLIENT_SECRET || '0000000000000000000000000000000000000000'
+  , ironKey      : process.env.IRON_KEYPASS
+  , isSecure     : process.env.GITHUB_FORCE_HTTPS || false
   }
-};
+}
