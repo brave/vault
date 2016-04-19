fc5e1414dc5a7fc7b86685009fcbf956d7ac0063
Marshall Rose <mrose17@gmail.com>
2016-04-18 20:17:42 -0700

    database update

---
+++ src/database.js
@@ -0,0 +1,44 @@
+var monk = require('monk')
+var underscore = require('underscore')
+
+var DB = function (config) {
+  if (!(this instanceof DB)) { return new DB(config) }
+
+  if (!config.database) throw new Error('config.database undefined')
+
+  if (config.database.mongo) config.database = config.database.mongo
+  this.db = monk(config.database)
+}
+
+DB.prototype.get = function (collection) {
+  return this.db.get(collection)
+}
+
+DB.prototype.checkIndices = async function (debug, entries) {
+  entries.forEach(async function (entry) {
+    var doneP, indices
+    var category = entry.category
+
+    try { indices = await category.indexes() } catch (ex) { indices = [] }
+    doneP = underscore.intersection(underscore.keys(indices), [entry.property + '_0', entry.property + '_1']).length !== 0
+
+    debug(entry.name + ' indices ' + (doneP ? 'already' : 'being') + ' created')
+    if (doneP) { return }
+
+    try {
+      if (indices.length === 0) { await category.insert(entry.empty) }
+
+      (entry.unique || []).forEach(async function (index) {
+        await category.index(index, { unique: true })
+      });
+
+      (entry.others || []).forEach(async function (index) {
+        await category.index(index, { unique: false })
+      })
+    } catch (ex) {
+      debug('unable to create ' + entry.name + ' ' + entry.property + ' index', ex)
+    }
+  })
+}
+
+module.exports = DB
