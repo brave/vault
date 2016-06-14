bd4088b352e6f4ed6516fbd5adf7e887243e982f
Marshall Rose <mrose17@gmail.com>
2016-06-13 18:01:33 -0700

    Merge branch 'integration'

--- src/index.js
+++ src/index.js
@@ -39,12 +39,10 @@ server.register(
   {
     register: require('hapi-swagger'),
     options: {
-      apiVersion: npminfo.version,
       info: {
         title: npminfo.name,
-        description: npminfo.description,
-        contact: npminfo.author,
-        license: npminfo.license
+        version: npminfo.version,
+        description: npminfo.description
       }
     }
   }
