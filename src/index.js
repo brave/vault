e0414be3bdd437de42313c61770161be58cc15be
Marshall Rose <mrose17@gmail.com>
2016-06-13 17:48:55 -0700

    use latest swagger

--- src/index.js
+++ src/index.js
@@ -37,12 +37,10 @@ server.register(
   {
     register: require('hapi-swagger'),
     options: {
-      apiVersion: npminfo.version,
       info: {
         title: npminfo.name,
-        description: npminfo.description,
-        contact: npminfo.author,
-        license: npminfo.license
+        version: npminfo.version,
+        description: npminfo.description
       }
     }
   }
