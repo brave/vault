d4ea940c7cd3599dfca70b5e080113e1cd286244
Marshall Rose <mrose17@gmail.com>
2016-08-16 14:45:34 -0700

    config check keys

--- src/runtime.js
+++ src/runtime.js
@@ -1,7 +1,21 @@
+var underscore = require('underscore')
+
 var DB = require('./database')
 
 var profile = process.env.NODE_ENV || 'development'
 var config = require('../config/config.' + profile + '.js')
+underscore.keys(config).forEach((key) => {
+  var m = config[key]
+  if (typeof m === 'undefined') return
+
+  underscore.keys(m).forEach((k) => {
+    if (typeof m[k] === 'undefined') throw new Error('config.' + key + '.' + k + ': undefined')
+
+    if ((typeof m[k] !== 'number') && (typeof m[k] !== 'boolean') && (!m[k])) {
+      throw new Error('config.' + key + '.' + k + ': empty')
+    }
+  })
+})
 
 module.exports = {
   config: config,
